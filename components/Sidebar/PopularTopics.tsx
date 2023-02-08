4ab404acffed61b0caca3d5b7c90bccb9efcf94c
Maher E. Elmashharawi <mashmaher96@gmail.com>
2023-02-08 13:04:23 +0200

    add google auth

--- components/Sidebar/PopularTopics.tsx
+++ components/Sidebar/PopularTopics.tsx
@@ -1,62 +1,62 @@
 import {
-  Stack,
-  Typography,
-  ToggleButtonGroup,
-  ToggleButton,
+	Stack,
+	Typography,
+	ToggleButtonGroup,
+	ToggleButton,
 } from "@mui/material";
 import { useRouter } from "next/router";
 import { popularTopics } from "../../utils/constants";
 import { TopicToggleLink } from "./index";
 
 const PopularTopics = () => {
-  const router = useRouter();
-  const { topic } = router.query;
+	const router = useRouter();
+	const { topic } = router.query;
 
-  return (
-    <Stack direction={"column"}>
-      <Typography
-        color="gray"
-        textTransform={"capitalize"}
-        fontWeight={500}
-        mb={"12px"}
-      >
-        Popular Topics
-      </Typography>
-      <ToggleButtonGroup
-        value={topic}
-        size="medium"
-        aria-label="Popular topics"
-        exclusive
-        sx={{
-          display: "flex",
-          flexDirection: { xs: "column", md: "row" },
-          flexWrap: "wrap",
-          gap: "10px",
-        }}
-      >
-        {popularTopics.map((topic) => {
-          const { name, icon } = topic;
-          return (
-            <ToggleButton
-              key={name}
-              value={name}
-              sx={{
-                color: "black",
-                transition: "all 0.3s ease-in-out",
-                "&:hover": { color: "#f51997" },
-                border: "1px solid gray",
-                borderLeft: "1px solid gray !important",
-                borderRadius: "25px !important",
-                p: "0px",
-              }}
-            >
-              <TopicToggleLink name={name} icon={icon} />
-            </ToggleButton>
-          );
-        })}
-      </ToggleButtonGroup>
-    </Stack>
-  );
+	return (
+		<Stack direction={"column"}>
+			<Typography
+				color="gray"
+				textTransform={"capitalize"}
+				fontWeight={500}
+				mb={"12px"}
+			>
+				Popular Topics
+			</Typography>
+			<ToggleButtonGroup
+				value={topic}
+				size="medium"
+				aria-label="Popular topics"
+				exclusive
+				sx={{
+					display: "flex",
+					flexDirection: { xs: "column", md: "row" },
+					flexWrap: "wrap",
+					gap: "10px",
+				}}
+			>
+				{popularTopics.map((topic) => {
+					const { name, icon } = topic;
+					return (
+						<ToggleButton
+							key={name}
+							value={name}
+							sx={{
+								color: "black",
+								transition: "all 0.3s ease-in-out",
+								"&:hover": { color: "#f51997" },
+								border: "1px solid gray",
+								borderLeft: "1px solid gray !important",
+								borderRadius: "25px !important",
+								p: "0px",
+							}}
+						>
+							<TopicToggleLink name={name} icon={icon} />
+						</ToggleButton>
+					);
+				})}
+			</ToggleButtonGroup>
+		</Stack>
+	);
 };
 
 export default PopularTopics;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,35 +7,40 @@ import theme from "../components/theme";
 import createEmotionCache from "../components/createEmotionCache";
 import { Navbar } from "../components";
 import "../styles/globale.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
+	emotionCache?: EmotionCache;
 }
 
 export default function MyApp(props: MyAppProps) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-  return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <meta
-          name="description"
-          content="Discover a world of videos with our all-in-one platform. Explore and enjoy videos from a wide range of categories - everything from music and entertainment to education and technology."
-        />
-        <title>Hilarity HQ</title>
-        <link rel="icon" href="./favicon.ico" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Navbar />
-        <main className={"paddingContent min-h-100vh max-w-1300px mx-auto"}>
-          <Component {...pageProps} />
-        </main>
-      </ThemeProvider>
-    </CacheProvider>
-  );
+	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+	return (
+		<GoogleOAuthProvider
+			clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
+		>
+			<CacheProvider value={emotionCache}>
+				<Head>
+					<meta name="viewport" content="initial-scale=1, width=device-width" />
+					<meta
+						name="description"
+						content="Discover a world of videos with our all-in-one platform. Explore and enjoy videos from a wide range of categories - everything from music and entertainment to education and technology."
+					/>
+					<title>Hilarity HQ</title>
+					<link rel="icon" href="./favicon.ico" />
+				</Head>
+				<ThemeProvider theme={theme}>
+					{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+					<CssBaseline />
+					<Navbar />
+					<main className={"paddingContent min-h-100vh max-w-1300px mx-auto"}>
+						<Component {...pageProps} />
+					</main>
+				</ThemeProvider>
+			</CacheProvider>
+		</GoogleOAuthProvider>
+	);
 }
