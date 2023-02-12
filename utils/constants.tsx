2a318e8727c6401e7771b45dcf7f3e7d2d153792
Maher E. Elmashharawi <mashmaher96@gmail.com>
2023-02-12 16:13:06 +0200

    remove category

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -2,67 +2,62 @@ import CodeOutlinedIcon from "@mui/icons-material/CodeOutlined";
 import SentimentVerySatisfiedOutlinedIcon from "@mui/icons-material/SentimentVerySatisfiedOutlined";
 import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
 import FastfoodOutlinedIcon from "@mui/icons-material/FastfoodOutlined";
-import NightlifeOutlinedIcon from "@mui/icons-material/NightlifeOutlined";
 import FaceRetouchingNaturalOutlinedIcon from "@mui/icons-material/FaceRetouchingNaturalOutlined";
 import PetsOutlinedIcon from "@mui/icons-material/PetsOutlined";
 import SurfingOutlinedIcon from "@mui/icons-material/SurfingOutlined";
 
 export const popularTopics = [
-  {
-    name: "coding",
-    icon: <CodeOutlinedIcon />,
-  },
-  {
-    name: "comedy",
-    icon: <SentimentVerySatisfiedOutlinedIcon />,
-  },
-  {
-    name: "gaming",
-    icon: <SportsEsportsOutlinedIcon />,
-  },
-  {
-    name: "food",
-    icon: <FastfoodOutlinedIcon />,
-  },
-  {
-    name: "dance",
-    icon: <NightlifeOutlinedIcon />,
-  },
-  {
-    name: "beauty",
-    icon: <FaceRetouchingNaturalOutlinedIcon />,
-  },
-  {
-    name: "animals",
-    icon: <PetsOutlinedIcon />,
-  },
-  {
-    name: "sports",
-    icon: <SurfingOutlinedIcon />,
-  },
+	{
+		name: "coding",
+		icon: <CodeOutlinedIcon />,
+	},
+	{
+		name: "comedy",
+		icon: <SentimentVerySatisfiedOutlinedIcon />,
+	},
+	{
+		name: "gaming",
+		icon: <SportsEsportsOutlinedIcon />,
+	},
+	{
+		name: "food",
+		icon: <FastfoodOutlinedIcon />,
+	},
+	{
+		name: "beauty",
+		icon: <FaceRetouchingNaturalOutlinedIcon />,
+	},
+	{
+		name: "animals",
+		icon: <PetsOutlinedIcon />,
+	},
+	{
+		name: "sports",
+		icon: <SurfingOutlinedIcon />,
+	},
 ];
 
 export const footerList1 = [
-  "About",
-  "Newsroom",
-  "Store",
-  "Contact",
-  "Carrers",
-  "ByteDance",
-  "Creator Directory",
+	"About",
+	"Newsroom",
+	"Store",
+	"Contact",
+	"Carrers",
+	"ByteDance",
+	"Creator Directory",
 ];
 export const footerList2 = [
-  "Hilarity HQ for Good",
-  "Advertise",
-  "Developers",
-  "Transparency",
-  "Hilarity HQ Rewards",
+	"Hilarity HQ for Good",
+	"Advertise",
+	"Developers",
+	"Transparency",
+	"Hilarity HQ Rewards",
 ];
 export const footerList3 = [
-  "Help",
-  "Safety",
-  "Terms",
-  "Privacy",
-  "Creator Portal",
-  "Community Guidelines",
+	"Help",
+	"Safety",
+	"Terms",
+	"Privacy",
+	"Creator Portal",
+	"Community Guidelines",
 ];
